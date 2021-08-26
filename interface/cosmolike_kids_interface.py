0a830cb0e0b4eb4d3ca4ad5cda54bdd3f1772d72
mariamutz <mariamutz2019@u.northwestern.edu>
2021-08-26 13:42:23 -0700

    Create cosmolike_kids_interface.py

---
+++ interface/cosmolike_kids_interface.py
@@ -0,0 +1,10 @@
+def __bootstrap__():
+   global __bootstrap__, __loader__, __file__
+   import sys, pkg_resources, imp
+   #FLAG
+   __file__ = pkg_resources.resource_filename(__name__,'cosmolike_des_y3_interface.so')
+   __loader__ = None; del __bootstrap__, __loader__
+   imp.load_dynamic(__name__,__file__)
+__bootstrap__()
+
+#also changed name--wasn't sure if the interface is something that can be left as-is or not
